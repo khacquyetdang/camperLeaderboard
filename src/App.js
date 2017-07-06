91360a8f5aac7ec2d8f26e9906fa49cdca83e6ae
mrdang <khacquyet.dang@gmail.com>
2017-07-06 22:53:41 +0200

    init project with

---
+++ src/App.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import './App.sass';
+
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      textOriginValue: '',
+      textMarkDownValue: ''
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({textOriginValue: event.target.value,
+      textMarkDownValue: event.target.value,
+    });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Mardown preview</h1>
+        <div class="flex-container">
+          <div class="flex-item">
+            <textarea className="textArea"
+              rows="20"
+              cols="40"
+              value={this.state.textOriginValue }
+              onChange={this.handleChange}/>
+          </div>
+          <div class="flex-item">
+            <p>
+              { this.state.textMarkDownValue }
+            </p>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
