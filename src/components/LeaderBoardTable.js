6bb85ed54451a6db06ace8dfb093e51bc2385eb3
mrdang <khacquyet.dang@gmail.com>
2017-07-19 10:20:37 +0200

    add sorting by recent score and all time score

--- src/components/LeaderBoardTable.js
+++ src/components/LeaderBoardTable.js
@@ -7,24 +7,71 @@ class LeaderBoardTable extends Component {
 
   constructor(props) {
     super(props);
+    console.log("constructor ");
+    console.log(this.props)
+    this.state = {
+      usersScore : this.props.usersScore
+    };
   }
 
+  componentWillReceiveProps(nextProps)
+  {
+    this.setState ({
+      usersScore : nextProps.usersScore
+    });
+  }
 
+  sortByLastThirdyDay()
+  {
+    var usersScoreArr = this.state.usersScore;
+    usersScoreArr.sort(function(user1, user2) {
+      return user2.recent - user1.recent;
+    });
+    this.setState ({
+      usersScore : usersScoreArr
+    });
+  }
+
+  sortByAllTime()
+  {
+    var usersScoreArr = this.state.usersScore;
+    usersScoreArr.sort(function(user1, user2) {
+      return user2.alltime - user1.alltime;
+    });
+    this.setState ({
+      usersScore : usersScoreArr
+    });
+  }
+
+  renderTableContent()
+  {
+    console.log("renderTableContent");
+    console.log(this.props);
+    const usersScoreArr = this.state.usersScore;
+    console.log(usersScoreArr);
+    if (usersScoreArr != null)
+    {
+      const rowsTable = usersScoreArr.map((userScore, index) => <LeaderBoardRow key={index} id={index} userScore={userScore} />);
+
+      return rowsTable;
+    }
+    return null;
+  }
   render() {
-    const usersScoreArr = this.props.usersScore;
 
     return (
       <div className="LeaderBoardContainer">
         <div className="LeaderBoardHeader">LeaderBoard</div>
         <table className="LeaderBoardTable">
-
-          <tr className="headerTableRow">
-            <th className="headerTableColumn"># </th>
-            <th className="headerTableColumn">Camper Name </th>
-            <th className="headerTableColumn">Points in past 30 days </th>
-            <th className="headerTableColumn">All time points </th>
-          </tr>
-          {usersScoreArr.map((userScore, index) => <LeaderBoardRow id={index} userScore={userScore} />)}
+          <tbody>
+            <tr className="headerTableRow">
+              <th className="headerTableColumn"># </th>
+              <th className="headerTableColumn">Camper Name </th>
+              <th className="headerTableColumn"><div onClick={(e) => this.sortByLastThirdyDay()}>Points in past 30 days</div> </th>
+              <th className="headerTableColumn"><div onClick={(e) => this.sortByAllTime()}>All time points </div></th>
+            </tr>
+            { this.renderTableContent()}
+          </tbody>
         </table>
     </div>
     );
